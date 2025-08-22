785ac2ddd458b8bdaf77173850fc3b1e362789ea
OmVarma9 <omvarma369@gmail.com>
2025-08-23 01:06:12 +0530

    Nav Bar done and Text added in hero section

---
+++ src/section/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroText from '../components/HeroText'
+
+const Hero = () => {
+  return (
+    <section className='flex-items items-start justify-center md:items-start min-h-screen overflow-hidden'>
+      <HeroText />
+    </section>
+  )
+}
+
+export default Hero
